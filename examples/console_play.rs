c06444779c0e661e4ef0725ec0638ac781a9d921
thanadolps <thanadolps@gmail.com>
2019-11-26 20:33:05 +0700

    Commit Missing Cargo

--- examples/console_play.rs
+++ examples/console_play.rs
@@ -1,28 +1,32 @@
-use simple_xo::*;
-use simple_xo::XOToken::*;
-use std::io::stdin;
+use bitboard_xo::*;
 use text_io::*;
+use std::io::{stdout, Write};
 
 fn main() -> Result<(), XOError> {
     let mut game = XO::new();
 
     loop {
+
+        // read index from console
         let index = loop {
+            print!("Input index for {}: ", game.turn());
+            stdout().flush().unwrap();
             match try_read!() {
                     Ok(a) => break a,
                     Err(b) => println!("{}", b),
             }
         };
 
-        assert!(game.board_ref().check_sanity());
+        // assert!(game.board_ref().check_sanity());
 
+        // move it and match returned game state
         match game.push_move(index) {
             Ok(Some(winner)) => {
                 println!("{}", game);
                 println!("winner = {:?}", winner);
                 break
             },
-            Ok(None) => println!("game continue"),
+            Ok(None) => println!("game continue..."),
             Err(xo_err) =>  println!("Error: {}", xo_err),
         }
 
